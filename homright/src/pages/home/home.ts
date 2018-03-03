6baa250712f0c68656bcd74551c1c2cde1c02b59
Unknown <oceanemariel@hotmail.fr>
2018-03-03 21:18:35 +0100

    Splashcreen
    Couleurs et animations à définir

--- homright/src/pages/home/home.ts
+++ homright/src/pages/home/home.ts
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tabBarElement: any;
+  splash = true;
 
+  constructor(public navCtrl: NavController) {
+    this.tabBarElement = document.querySelector('.tabbar');
   }
+
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      this.tabBarElement.style.display = 'flex';
+    }, 5000);
+  }
+
 
 }
