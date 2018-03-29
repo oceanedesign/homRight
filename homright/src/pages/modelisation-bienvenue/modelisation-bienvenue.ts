787dc7324791c8bc771fe7f3f27cfc4b9149a019
Emma <vanhoorde.em@gmail.com>
2018-03-29 16:56:55 +0200

    Modélisation Bienvenue ok

--- homright/src/pages/modelisation-bienvenue/modelisation-bienvenue.ts
+++ homright/src/pages/modelisation-bienvenue/modelisation-bienvenue.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { JeuModelisationPage } from '../jeu-modelisation/jeu-modelisation';
+import { HomePage } from '../home/home';
+
 /**
  * Generated class for the ModelisationBienvenuePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+
+ declare var JQuery:any;
+ declare var $:any;
+ import 'jquery-ui-dist/jquery-ui';
 
 @IonicPage()
 @Component({
@@ -20,6 +27,18 @@ export class ModelisationBienvenuePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModelisationBienvenuePage');
+  }
+  modeliserMaison(){
+    this.navCtrl.push(JeuModelisationPage);
+  }
+  pasModeliserMaison(){
+    this.navCtrl.push(HomePage);
+  }
+  plusTard(){
+    $(".fond-cache").css("display", "flex");
+  }
+  returnChoose(){
+    $(".fond-cache").css("display", "none");
   }
 
 }
