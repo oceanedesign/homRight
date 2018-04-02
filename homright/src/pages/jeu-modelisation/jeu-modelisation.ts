5943fb654bb83d7a1208e073b9b6a4290b875b9e
Unknown <oceanemariel@hotmail.fr>
2018-04-02 20:07:36 +0200

    Jeu : drag and drop okay

--- homright/src/pages/jeu-modelisation/jeu-modelisation.ts
+++ homright/src/pages/jeu-modelisation/jeu-modelisation.ts
@@ -24,6 +24,7 @@ export class JeuModelisationPage {
 cloneCount = 1;
 ligne: number = 5;
 colonne:number=5;
+PosTotal:any;
 
   constructor(public plt: Platform, private screenOrientation : ScreenOrientation, public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -39,16 +40,24 @@ colonne:number=5;
       console.log('I am a core device!');
     }
      // console.log("test .js : "+colonnes);
+    this.PosTotal = $(".ligne-sol").offset(); 
+    console.log("top : "+this.PosTotal.top+" + left : "+ this.PosTotal.left);
     this.tailleSol();
     this.MaisonDeplace();
     this.ActiveDraggable();
-
+        
   }
   
 
   MaisonDeplace(){
-    $(".case-sol")
-      .draggable({ grid: [ 60, 60] })
+    $(".ligne-sol")
+      .draggable({
+      grid: [ 60, 60],
+       stop: function( event, ui ) {       
+        this.PosTotal = $(this).offset();                
+        console.log("top : "+this.PosTotal.top+" + left : "+ this.PosTotal.left);
+      }
+      })
       
       .on("mouseover", function(){
         $( this ).addClass("move-cursor")
@@ -68,91 +77,94 @@ colonne:number=5;
           .removeClass("opac")
           .addClass("move-cursor");
       });
+
   }
 
 
 
-  ActiveDraggable(){
-
-    $( ".spare-item" ).draggable({
-        grid: [ 10, 10 ],
-        containment: "#preview",
-        
-        cursor: "grab",
-        revert: 'invalid',
-        stop: function(){
-        $(this).draggable('option','revert','invalid');
-    },
-       start: function (event, ui) { 
-      //if (outside==true) {
-        $(this).removeClass("spare-item");
-        $(this).addClass("spare-item2");
-      //}
-    }});
-                 
-    $('.spare-item2').droppable({
-      greedy: true,
-      tolerance: 'intersect',
-      drop: function(event,ui){
-          ui.draggable.draggable('option','revert',true);
-      }
-    });
-            
+  ActiveDraggable(){           
     $( "#snaptarget" ).droppable({
       accept: ".spare-item2",
       over: function(event, ui) {
           $(".spare-item2").draggable({
               grid: [10, 10]
           });
-      },    
-      drop: function( event, ui ) {
-          var pos = ui.draggable.offset(), dPos = $(this).offset();
-          console.log(pos.top - dPos.top, pos.left - dPos.left);
       }
-    });            
+
+    });
+
   }; 
 
 
 
 
-  changeLigneColonne(){
+  previewObjet(){
     var enfantCaseSolPreview = $('.actions').find('.spare-item'); //Definit la variable
+    var countDrag =0;
     if(enfantCaseSolPreview.length > 0) //
       { 
         $('.actions').find('.spare-item').remove(); // Suppression de l'enfant
       }
 
-    $( ".objets-presentation" ).clone().removeClass( "objets-presentation" ).appendTo(".actions").addClass("spare-item").draggable({
+    $( ".objets-presentation" ).clone().removeClass( "objets-presentation" ).appendTo(".actions").addClass("spare-item")
+    .draggable({
       //grid: [ 10, 10 ],
       // containment: "#preview",
       cursor: "grab",
+      cursorAt: { left: 0, top:0 },
       revert: 'invalid',
-      stop: function(){
-        $(this).draggable('option','revert','invalid');
+      drag: function (event, ui) {
+        //$(this).draggable( "option", "refreshPositions", true );
+        //console.log("top : "+ui.offset.top+" + left : "+ui.offset.left);
+       if(countDrag==1){
+        this.PosTotal = $(".ligne-sol").offset();
+        ui.position.left = ui.position.left - this.PosTotal.left;   
+        ui.position.top = ui.position.top - this.PosTotal.top;
+      }      
+        //$(this).offset({
+        //top : ui.offset.top -80,
+        //left:  ui.offset.left -80
+      //});
+
+
       },
       start: function (event, ui) { 
+        countDrag++;
         //if (outside==true) {
         $(this).removeClass("spare-item");
-        $(this).addClass("spare-item2").droppable({
-          greedy: true,
-          tolerance: 'intersect',
-          drop: function(event,ui){
-            ui.draggable.draggable('option','revert',true);
-          }
-        });
-      //}
-      }
-    });
+        $(this).addClass("spare-item2");
 
+       // $(this).css({
+        //  left: $(this).position().left - this.PosTotal.left,
+        //  top: $(this).position().top - this.PosTotal.top
+        //});
+        //
+      //}
+
+        //$(event.target).data('offset-x', "+" + ui.offset.left);
+       // $(event.target).data('offset-y', "+" + ui.offset.top);      
+      },
+      beforeStop:function(event,ui){
+        
+       // console.log("test");
+      },
+
+      stop: function(){
+        $(this).insertBefore("#preview");
+        $(this).draggable('option','revert','invalid');
+        
+      }
+
+    });
       //$( "#draggable5" ).clone().attr('id', 'id'+ this.cloneCount++).appendTo(".droptarget").css({top: this.ligne*80, left: this.colonne*80});
-      
   }
 
   tailleSol(){
     console.log("colonne : "+ this.colonne);
-    $('.case-sol').width(this.colonne*60-2+"px");
-    $('.case-sol').height(this.ligne*60-2+"px");
-
+    $('.case-sol, .actions').width(this.colonne*160-2+"px");
+    $('.case-sol, #preview, .actions').height(this.ligne*60-2+"px");
+    $('#preview').width(this.colonne*60-2+"px");
+    //$('#preview').height(this.ligne*60-2+"px");
   }
 
   getCurrentScreenOrientation(){
