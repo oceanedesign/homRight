9570f75c3f398334de4693803fad9a1a69a34c9c
Emma <vanhoorde.em@gmail.com>
2018-04-13 18:18:29 +0200

    Auto stash before merge of "master" and "origin/master"

--- homright/src/app/app.component.ts
+++ homright/src/app/app.component.ts
@@ -20,8 +20,9 @@ import { PreModelisationPage } from '../pages/pre-modelisation/pre-modelisation'
 export class MyApp {
 
   //rootPage:any = JeuModelisationPage;
-  rootPage:any = LoginPage;
+  //rootPage:any = LoginPage;
   //rootPage:any = PreModelisationPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
