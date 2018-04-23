4033878f93a8cd2ca116a7e2a80d5936b16221e7
Unknown <oceanemariel@hotmail.fr>
2018-04-23 23:15:06 +0200

    Correction

--- homright/src/app/app.component.ts
+++ homright/src/app/app.component.ts
@@ -20,7 +20,7 @@ export class MyApp {
 
   //rootPage:any = LoginPage;
   //rootPage:any = PreModelisationPage;
-  //rootPage:any = HomePage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
