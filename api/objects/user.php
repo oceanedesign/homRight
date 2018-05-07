e928bdc7efa997ce046594396efbf5a529d88df9
root <root@debian.debianlocal>
2018-05-07 07:46:33 +0200

    create + get user

--- api/objects/user.php
+++ api/objects/user.php
@@ -8,78 +8,119 @@
 /**
  * Description of user
  *
- * @author co59
  */
-include_once('../errors.php');
-
-header('Access-Control-Allow-Origin:*');
-	header('Access-Control-Allow-Origin:*');
-
-	header('Content-Type: application/json;charset=UTF-8');
-	header('Access-Control-Allow-Methods: DELETE, HEAD, GET, OPTIONS, POST, PUT');
-	header('Access-Control-Allow-Headers: : Origin, Content-Type, X-Auth-Token , Authorization');
-	header('Access-Control-Max-Age: 1728000');
+require_once('../errors.php');
 
 class User {
     //Variables privées
     private $db_connection  = null;
     private $table_name     = "user";
-    private $properties     = array();
-    
+
     //Variables publiques
-    public $columns         = array();
+    public $properties     = array();
     
+    //-----METHODES PRIVEES
+    private function _init_properties() {
+        $stmt = $this->db_connection->get_columns_by_table_name($this->table_name);
+        
+        for ($i = 0; $i < $stmt->rowCount(); $i++) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_ABS, $i);
+            
+            // Ignorer le champ date_creation -> rempli automatiquement par mysql
+            if($row["Field"] == "date_creation") {
+                continue;
+            }
+            
+            $this->properties[$row["Field"]] = "";
+        }
+    }
+    
+    private function _generate_token($strong) {
+        return bin2hex(openssl_random_pseudo_bytes(128, $strong));
+    }
+    //-----FIN METHODES PRIVEES
+    
+//-----METHODES PUBLIQUES
     public function __construct($database) {
         $this->db_connection = $database;
-        
-        //Récupérer les colonnes de la table
-        // /!\ table_name doit être hardcodé -> risque d'injection sql
-        $req = "show columns from " . $this->table_name;
-        $result = $this->connection->query($req);
-        
-        if(! $result) {
-            return errors("db_SendRequest", "Erreur lors de la récupération des colonnes");
-        }
-        
-        while ($row = $result->fetch_assoc()) {
-            $this->columns[] = $row["Field"];
-            $this->properties[$row["Field"]] = null;
-        }
-        $result->free();
+        $this->PDO_object = $database->get_connection();
+        $this->_init_properties();
     }
     
     public function set_property($name, $value) {
-        if (! array_key_exists($name, $this->properties)) {
-            return errors("UnknownProperty", "une propriété n'existe pas");
+        if (!in_array($name, array_keys($this->properties))) {
+            return errors("User_setproperty", "La propriété $name n'existe pas");
         }
-        //TODO: Filter/valider les informations de $value
-        $this->properties[$name] = $value;
         
-        return true;
+        $this->properties[$name] = $value; 
     }
     
     public function create() {
-        $fields = array("pseudo", "nom", "prenom", "password", "email", "date_creation", "longitude", "latitude", "point", "point_niv", "email_n", "push");
-        $request = "INSERT INTO user(pseudo, nom, prenom, password, email, date_creation, longitude, latitude, point, point_niv, email_n, push) VALUES('$pseudo', '$nom', '$prenom', '$password', '$email', '$date_creation','$longitude','$latitude', 50, 50, 1, 1)";
+        //Fixer les valeurs par défaut
+        $default_values = array("email_n" => 1, "push" => 1, "point" => 50, "point_niv" => 50, "token" => $this->_generate_token(true));
+        $this->properties = array_merge($this->properties, $default_values);
         
-        //Prépare la requête
-        if (! $stmt = mysqli_prepare($this->db_connection, $request)) {
-            return errors("db_PrepareRequest", "Erreur lors de la préparation d'une requête");
+        //Hash le mot de passe avant de l'insérer dans la base
+        $this->properties["password"] = hash("sha1", $this->properties["password"]);
+        
+        $request = $this->db_connection->build_insert_request($this->table_name, $this->properties);
+        
+        //Préparer la requête
+        try {
+            $stmt = $this->PDO_object->prepare($request);
+        } catch (PDOException $ex) {
+            return errors("PDOException", $ex->getMessage());
         }
         
-        //Affectuer les valeurs dans la requête
-        foreach ($fields as $field) {
-            if(! $stmt->bind_param("s", $this->properties[$field])) {
-                return errors("db_BindParam", "Erreur lors de préparation d'une requête");
+        if(! $stmt) {
+            return errors("User_prepare", "Erreur lors de la préparation de la requête $request");
+        }
+
+        //Lier les données dans la requête
+        foreach(array_keys($this->properties) as $column) {
+            if(! $stmt->bindParam(":$column", $this->properties[$column])) {
+                return errors("User_bindParam", "Erreur lors de la liaison de donnée");
             }
         }
-            
+        
         //Executer la requête
         if (! $stmt->execute()) {
-            return errors("db_Execute", "Erreur lors de  l'execution d'une requête");
+            return errors("User_execute", "Erreur lors de l'execution d'une requete");
         }
         
-        $stmt->close();
-        return true;
-    }        
+        return array("token" => $this->properties["token"]);
+    }   
+    
+    public function get_by_pseudo_and_password($pseudo, $passwd) {
+        $passwd = hash("sha1", $passwd);
+        $request = "SELECT * from $this->table_name WHERE pseudo=:pseudo AND password=:password";
+        
+        //Préparer la requête
+        try {
+            $stmt = $this->PDO_object->prepare($request);
+        } catch (PDOException $ex) {
+            return errors("PDOException", $ex->getMessage());
+        }
+        
+        if(! $stmt) {
+            return errors("User_prepare", "Erreur lors de la préparation de la requête $request");
+        }
+        
+        if (! ($stmt->bindParam(":pseudo", $pseudo) and $stmt->bindParam(":password", $passwd))) {
+                return errors("User_bindParam", "Erreur lors de la liaison de donnée");
+        }
+        
+        //Executer la requête
+        if (! $stmt->execute()) {
+            return errors("User_execute", "Erreur lors de l'execution d'une requete");
+        }
+        
+        //Vérifier qu'il existe 1 élément
+        if ($stmt->rowCount() != 1) {
+            return errors("User_count", "Le pseudo [$pseudo] n'existe pas");
+        }
+        
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    //-----FIN METHODES PUBLIQUES
 }
