b604bee5e749e6d35e0e687e4029f970fb952ff0
root <root@debian.debianlocal>
2018-05-13 15:48:17 +0200

    retourne le pseudo

--- api/users/create.php
+++ api/users/create.php
@@ -3,11 +3,13 @@ require_once("../config/database.php");
 require_once("../objects/user.php");
 require_once("../errors.php");
 
+/*
 header('Access-Control-Allow-Origin:*'); 
 header('Content-Type: application/json;charset=UTF-8'); 
 header('Access-Control-Allow-Methods: DELETE, HEAD, GET, OPTIONS, POST, PUT'); 
 header('Access-Control-Allow-Headers: Origin, Content-Type, Token, Authorization'); 
 header('Access-Control-Max-Age: 1728000'); 
+*/
 
 //Initialiser la connexion
 $db = new Database();
@@ -42,5 +44,7 @@ foreach (array_keys($user->properties) as $column) {
 
 $ret = $user->create();
 check_error($ret);
+
+$ret["pseudo"] = htmlentities($json_data["pseudo"]);
 
 success("Utilisateur créé", $options=$ret);
